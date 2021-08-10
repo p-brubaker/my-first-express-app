016dbb350af4164e1b370745901fed779cd37793
Paul Brubaker <pbrubak@gmail.com>
2021-08-10 13:21:47 -0700

    add res and req

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ My first express app
 
 1. Create a new repo in Github with a README and clone
 2. Run 'npm init -y' -- creates a 'package.json file -- go ahead and acp
+3. Install Express App dependencies with 'npm i express supertest jest cors'
+4. Add a .gitignore and add 'node_modules'
---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('Hi!');
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send(`Puppies are cute! Especially ${req.query.name}`);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})

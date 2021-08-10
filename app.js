9426de8b19212f6958f4d04fff0b50a4bc800986
Paul Brubaker <pbrubak@gmail.com>
2021-08-10 16:05:10 -0700

    add data to data.js

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log('Hi!');
+const cors = require('cors');
+app.use(cors());
+
+const express = require('express');
+const app = express();
+const data = require('./data.js');
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+app.get('/scientists', (req, res) => {
+    res.json(data);
+})
+
+app.get('/scientists/:id', (req, res) => {
+    const scientistId = Number(req.params.id);
+    const scientist = data.find((item) => item.id === scientistId)
+    console.log(scientist);
+    res.json(scientist);
+})
+
+module.exports = app;
---
+++ data.js
@@ -0,0 +1,34 @@
+const data = [
+    {
+        id: 1,
+        name: 'Ada Lovelace',
+        living: false,
+    },
+    {
+        id: 2,
+        name: 'Alan Turing',
+        living: false
+    },
+    {
+        id: 3,
+        name: 'Donald Knuth',
+        living: true
+    },
+    {
+        id: 4,
+        name: 'Yukihiro Matsumoto',
+        living: true
+    },
+    {
+        id: 5, 
+        name: 'Sophie Wilson',
+        living: true
+    },
+    {
+        id: 6,
+        name: 'Haskell Curry',
+        living: false
+    }
+];
+
+module.exports = data;
